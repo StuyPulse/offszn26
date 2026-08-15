a7771f7c4c5d8c393b2855a7f35c8eb6e1f2ee7e
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-08-14 22:50:47 -0400

    chore: remove every camera from the list as we don't know what they are yet

--- src/main/java/com/stuypulse/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/com/stuypulse/robot/subsystems/vision/VisionConstants.java
@@ -39,59 +39,18 @@ public interface VisionConstants {
   }
 
   /**
-   * Change: Remove hardcoded camera names (e.g. camera0Name, camera1Name, camera2Name)
-   *
-   * <p>Now uses a record to define every camera with its name, camera offset to the robot, and
-   * standard deviation factor.
-   *
-   * <p>This allows for easier management of cameras and also being the ONE source of truth.
-   *
    * <p>!!!! Note that the index of the Camera enum and the index of the camera data array must
    * match!!!!
    */
   enum Camera {
-    RIGHT,
-    LEFT,
-    BACK
+    // Add names here for each camera, in the order they are defined in the CamerasList.CAMERAS array
   }
 
   record CameraData(String name, Transform3d robotToCamera, double stdDevFactor) {}
 
   public interface CamerasList {
     final CameraData[] CAMERAS = {
-      new CameraData(
-          "limelight-right",
-          new Transform3d(
-              Units.inchesToMeters(-9.149),
-              Units.inchesToMeters(15.080),
-              Units.inchesToMeters(8.088),
-              new Rotation3d(
-                  Units.degreesToRadians(180),
-                  Units.degreesToRadians(28.0),
-                  Units.degreesToRadians(-80.203885))),
-          1.0),
-      new CameraData(
-          "limelight-left",
-          new Transform3d(
-              Units.inchesToMeters(-2.490),
-              Units.inchesToMeters(-14.8620),
-              Units.inchesToMeters(5.676),
-              new Rotation3d(
-                  Units.degreesToRadians(0),
-                  Units.degreesToRadians(14.955812),
-                  Units.degreesToRadians(71.5))),
-          1.0),
-      new CameraData(
-          "limelight-back",
-          new Transform3d(
-              Units.inchesToMeters(-10.676),
-              Units.inchesToMeters(-12.969),
-              Units.inchesToMeters(8.753),
-              new Rotation3d(
-                  Units.degreesToRadians(0),
-                  Units.degreesToRadians(27.875),
-                  Units.degreesToRadians(185.155825))),
-          1.0)
+    
     };
   }
 }
