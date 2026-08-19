c8695bdb23190f91c8f972e74e29c5938cbb413c
aidenzhang21345 <aidenzhang211@gmail.com>
2026-08-19 10:46:38 -0400

    fix + feat: use autologged shooter inputs, add shooter tolerance check, name shooter commands

--- src/main/java/com/stuypulse/robot/subsystems/shooter/Shooter.java
+++ src/main/java/com/stuypulse/robot/subsystems/shooter/Shooter.java
@@ -1,81 +1,94 @@
 package com.stuypulse.robot.subsystems.shooter;
 
-import java.util.function.Supplier;
-
-import org.littletonrobotics.junction.AutoLogOutput;
-
 import com.stuypulse.robot.constants.GlobalSettings;
-import com.stuypulse.robot.subsystems.shooter.ShooterIO.ShooterIOInputs;
+import com.stuypulse.robot.subsystems.shooter.ShooterConstants.ShooterSettings;
 import com.stuypulse.robot.subsystems.shooter.ShooterIO.ShooterIOOutputs;
+import com.stuypulse.robot.subsystems.shooter.ShooterIO.ShooterMode;
 import com.stuypulse.robot.util.FullSubsystem;
 import com.stuypulse.robot.util.InterpolationCalculator;
-import com.stuypulse.robot.subsystems.shooter.ShooterIO.ShooterMode;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.units.measure.*;
-
 import edu.wpi.first.wpilibj2.command.Command;
 
+import static edu.wpi.first.units.Units.RPM;
+
+import java.util.function.Supplier;
+import org.littletonrobotics.junction.AutoLogOutput;
+
 public class Shooter extends FullSubsystem {
-    private final ShooterIO io;
-    private final ShooterIOInputs inputs;
-    private final ShooterIOOutputs outputs;
+  private final ShooterIO io;
+  private final ShooterIOInputsAutoLogged inputs;
+  private final ShooterIOOutputs outputs;
 
-    @AutoLogOutput(key = "Shooter/State")
-    private ShooterState state; 
+  @AutoLogOutput(key = "Shooter/State")
+  private ShooterState state;
 
-    private final Supplier<Pose2d> poseSupplier;
+  private final Supplier<Pose2d> poseSupplier;
 
-    public enum ShooterState {
-        STOP,
-        SHOOT,
-        FERRY
+  private boolean atTolerance;
+
+  public enum ShooterState {
+    STOP,
+    SHOOT,
+    FERRY
+  }
+
+  public Shooter(ShooterIO io, Supplier<Pose2d> poseSupplier) {
+    this.state = ShooterState.STOP;
+    this.io = io;
+    this.inputs = new ShooterIOInputsAutoLogged();
+    this.outputs = new ShooterIOOutputs();
+
+    this.poseSupplier = poseSupplier;
+  }
+
+  private void setTargetVelocity(AngularVelocity targetVelocity) {
+    outputs.targetVelocity = targetVelocity;
+    outputs.mode = ShooterMode.VELOCITY_TORQUE_CURRENT_FOC;
+
+    atTolerance = inputs.topLeftMotorInputs.velocity.minus(targetVelocity).abs(RPM) < ShooterSettings.TOLERANCE.in(RPM);
+  }
+
+  private void setState(ShooterState state) {
+    this.state = state;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+
+    if (!GlobalSettings.EnabledSubsystems.SHOOTER.get()) {
+      outputs.mode = ShooterMode.STOP;
+      return;
     }
 
-    public Shooter(ShooterIO io, Supplier<Pose2d> poseSupplier) {
-        this.state = ShooterState.STOP;
-        this.io = io;
-        this.inputs = new ShooterIOInputs();    
-        this.outputs = new ShooterIOOutputs();
-
-        this.poseSupplier = poseSupplier;
+    switch (state) {
+      case STOP -> outputs.mode = ShooterMode.STOP;
+      case SHOOT -> setTargetVelocity(
+          InterpolationCalculator.getInterpolatedShotRPM(poseSupplier.get()));
+      case FERRY -> setTargetVelocity(
+          InterpolationCalculator.getInterpolatedFerryRPM(poseSupplier.get()));
     }
+  }
 
-    private void setTargetVelocity(AngularVelocity targetVelocity) {
-        outputs.targetVelocity = targetVelocity;
-        outputs.mode = ShooterMode.VELOCITY_TORQUE_CURRENT_FOC;
-    }
+  @Override
+  public void periodicAfterScheduler() {
+    io.applyOutputs(outputs);
+  }
 
-    private void setState(ShooterState state) {
-        this.state = state;
-    }
+  public boolean atTolerance() {
+    return atTolerance;
+  }
 
-    @Override
-    public void periodic() {
-        io.updateInputs(inputs);
+  public Command setShooting() {
+    return runOnce(() -> setState(ShooterState.SHOOT)).withName("Shooter Shoot");
+  }
 
-        if (!GlobalSettings.EnabledSubsystems.SHOOTER.get()) {
-            outputs.mode = ShooterMode.STOP;
-            return;
-        }
+  public Command setFerrying() {
+    return runOnce(() -> setState(ShooterState.FERRY)).withName("Shooter Ferry");
+  }
 
-        switch (state) {
-            case STOP -> outputs.mode = ShooterMode.STOP;
-            case SHOOT -> setTargetVelocity(InterpolationCalculator.getInterpolatedShotRPM(poseSupplier.get()));
-            case FERRY -> setTargetVelocity(InterpolationCalculator.getInterpolatedFerryRPM(poseSupplier.get()));
-        }
-    }
-
-    @Override
-    public void periodicAfterScheduler() {
-        io.applyOutputs(outputs);
-    }
-
-    public Command setShooting() {
-        return runOnce(() -> setState(ShooterState.SHOOT));
-    }
-
-    public Command setFerrying() {
-        return runOnce(() -> setState(ShooterState.FERRY));
-    }
+  public Command setStop() {
+    return runOnce(() -> setState(ShooterState.STOP)).withName("Shooter Stop");
+  }
 }
--- src/main/java/com/stuypulse/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/com/stuypulse/robot/subsystems/shooter/ShooterConstants.java
@@ -1,74 +1,77 @@
 package com.stuypulse.robot.subsystems.shooter;
 
 import static edu.wpi.first.units.Units.*;
-import edu.wpi.first.units.measure.*;
 
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.stuypulse.robot.util.config.TalonFXConfig;
+import edu.wpi.first.units.measure.*;
 
 public interface ShooterConstants {
-    public interface ShooterSettings {
-        // dervied directly from the cad from the teeth of the pullies, but not confirmed
-        // only used the right side, not sure if left is the same
-        double LEFT_GEARING = 30.0 / 48.0;
-        double RIGHT_GEARING = 30.0 / 48.0;
-        
-        // derived from the cad, but not confirmed
-        Mass DRUM_MASS = Pounds.of(6.05);
-        Distance DRUM_RADIUS = Inches.of(2.0);
-        MomentOfInertia SHOOTER_MOI = KilogramSquareMeters.ofBaseUnits(
+  public interface ShooterSettings {
+    // dervied directly from the cad from the teeth of the pullies, but not confirmed
+    // only used the right side, not sure if left is the same
+    double LEFT_GEARING = 30.0 / 48.0;
+    double RIGHT_GEARING = 30.0 / 48.0;
+
+    // derived from the cad, but not confirmed
+    Mass DRUM_MASS = Pounds.of(6.05);
+    Distance DRUM_RADIUS = Inches.of(2.0);
+    MomentOfInertia SHOOTER_MOI =
+        KilogramSquareMeters.ofBaseUnits(
             DRUM_MASS
                 .times(DRUM_RADIUS)
                 .times(DRUM_RADIUS)
                 .times(0.5)
-                .baseUnitMagnitude()
-        ); // I = 1/2 * m * r^2
-    }
+                .baseUnitMagnitude()); // I = 1/2 * m * r^2
 
-    public interface ShooterIds { // placeholders
-        int TOP_LEFT_MOTOR = 1;
-        int BOTTOM_LEFT_MOTOR = 2;
-        int TOP_RIGHT_MOTOR = 3;
-        int BOTTOM_RIGHT_MOTOR = 4;
-    }
+    // TODO: Needs tuning
+    AngularVelocity TOLERANCE = RPM.of(100);
+  }
 
-    public interface ShooterGains { // placeholders
-        // pid
-        double kP = 0.1;
-        double kI = 0.0;
-        double kD = 0.0;
-        
-        // ff
-        double kS = 0.0;
-        double kV = 0.0;
-        double kA = 0.0;
-    }
+  public interface ShooterIds { // placeholders
+    int TOP_LEFT_MOTOR = 1;
+    int BOTTOM_LEFT_MOTOR = 2;
+    int TOP_RIGHT_MOTOR = 3;
+    int BOTTOM_RIGHT_MOTOR = 4;
+  }
 
-    public interface ShooterMotorConfigs {
-        final TalonFXConfig LEFT_CONFIG = 
-            new TalonFXConfig()
-                .withNeutralMode(NeutralModeValue.Coast)
-                .withSupplyCurrentLimitEnabled(true)
-                .withSupplyCurrentLimitAmps(50.0) // placeholder
-                .withStatorCurrentLimitEnabled(true)
-                .withStatorCurrentLimitAmps(100.0) // placeholder
-                .withLowerLimitSupplyCurrent(40.0, 0.25) // placeholder
-                .withPIDConstants(ShooterGains.kP, ShooterGains.kI, ShooterGains.kD, 0)
-                .withFFConstants(ShooterGains.kS, ShooterGains.kV, ShooterGains.kA, 0)
-                .withSensorToMechanismRatio(ShooterSettings.LEFT_GEARING);
+  public interface ShooterGains { // placeholders
+    // pid
+    double kP = 0.1;
+    double kI = 0.0;
+    double kD = 0.0;
 
-        final TalonFXConfig RIGHT_CONFIG =
-            new TalonFXConfig()
-                .withNeutralMode(NeutralModeValue.Coast)
-                .withSupplyCurrentLimitEnabled(true)
-                .withSupplyCurrentLimitAmps(50.0) // placeholder
-                .withStatorCurrentLimitEnabled(true)
-                .withStatorCurrentLimitAmps(100.0) // placeholder
-                .withLowerLimitSupplyCurrent(40.0, 0.25) // placeholder
-                .withPIDConstants(ShooterGains.kP, ShooterGains.kI, ShooterGains.kD, 0)
-                .withFFConstants(ShooterGains.kS, ShooterGains.kV, ShooterGains.kA, 0)
-                .withInvertedValue(InvertedValue.Clockwise_Positive) // placeholder
-                .withSensorToMechanismRatio(ShooterSettings.RIGHT_GEARING);
-    }
+    // ff
+    double kS = 0.0;
+    double kV = 0.0;
+    double kA = 0.0;
+  }
+
+  public interface ShooterMotorConfigs {
+    final TalonFXConfig LEFT_CONFIG =
+        new TalonFXConfig()
+            .withNeutralMode(NeutralModeValue.Coast)
+            .withSupplyCurrentLimitEnabled(true)
+            .withSupplyCurrentLimitAmps(50.0) // placeholder
+            .withStatorCurrentLimitEnabled(true)
+            .withStatorCurrentLimitAmps(100.0) // placeholder
+            .withLowerLimitSupplyCurrent(40.0, 0.25) // placeholder
+            .withPIDConstants(ShooterGains.kP, ShooterGains.kI, ShooterGains.kD, 0)
+            .withFFConstants(ShooterGains.kS, ShooterGains.kV, ShooterGains.kA, 0)
+            .withSensorToMechanismRatio(ShooterSettings.LEFT_GEARING);
+
+    final TalonFXConfig RIGHT_CONFIG =
+        new TalonFXConfig()
+            .withNeutralMode(NeutralModeValue.Coast)
+            .withSupplyCurrentLimitEnabled(true)
+            .withSupplyCurrentLimitAmps(50.0) // placeholder
+            .withStatorCurrentLimitEnabled(true)
+            .withStatorCurrentLimitAmps(100.0) // placeholder
+            .withLowerLimitSupplyCurrent(40.0, 0.25) // placeholder
+            .withPIDConstants(ShooterGains.kP, ShooterGains.kI, ShooterGains.kD, 0)
+            .withFFConstants(ShooterGains.kS, ShooterGains.kV, ShooterGains.kA, 0)
+            .withInvertedValue(InvertedValue.Clockwise_Positive) // placeholder
+            .withSensorToMechanismRatio(ShooterSettings.RIGHT_GEARING);
+  }
 }
