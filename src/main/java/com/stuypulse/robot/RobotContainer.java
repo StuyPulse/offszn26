fd6588ade7b10db8be6e8be3ffdd828eb0beb1b1
aidenzhang21345 <aidenzhang211@gmail.com>
2026-08-19 17:09:16 -0400

    feat: set limelight camera's pose in robot space

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -7,10 +7,8 @@ package com.stuypulse.robot;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.stuypulse.robot.commands.DriveCommands;
 import com.stuypulse.robot.constants.DriverConstants;
-
 import com.stuypulse.robot.constants.GlobalSettings;
 import com.stuypulse.robot.constants.GlobalSettings.VisionMode;
-
 import com.stuypulse.robot.subsystems.swerve.GyroIO;
 import com.stuypulse.robot.subsystems.swerve.GyroIOReal;
 import com.stuypulse.robot.subsystems.swerve.ModuleIO;
@@ -24,16 +22,13 @@ import com.stuypulse.robot.subsystems.vision.VisionIO;
 import com.stuypulse.robot.subsystems.vision.VisionIOLimelight;
 import com.stuypulse.robot.subsystems.vision.VisionIOPhotonVision;
 import com.stuypulse.robot.subsystems.vision.VisionIOPhotonVisionSim;
-
 import com.stuypulse.robot.util.InterpolationCalculator;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-
 import java.util.Arrays;
-
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -67,19 +62,24 @@ public class RobotContainer {
                 new ModuleIOReal(TunerConstants.BackRight));
 
         if (GlobalSettings.VISION_MODE == VisionMode.LIMELIGHT_VISION) {
-            vision = new Vision(
-                swerve,
-                    Arrays.stream(CamerasList.CAMERAS)
-                        .map((camera) -> new VisionIOLimelight(camera.name(), swerve::getRotation))
-                        .toArray(VisionIO[]::new)
-            );
+          vision =
+              new Vision(
+                  swerve,
+                  Arrays.stream(CamerasList.CAMERAS)
+                      .map(
+                          (camera) ->
+                              new VisionIOLimelight(
+                                  camera.name(), camera.robotToCamera(), swerve::getRotation))
+                      .toArray(VisionIO[]::new));
         } else {
-            vision = new Vision(
-                swerve,
-                    Arrays.stream(CamerasList.CAMERAS)
-                        .map((camera) -> new VisionIOPhotonVision(camera.name(), camera.robotToCamera()))
-                        .toArray(VisionIO[]::new)
-            );
+          vision =
+              new Vision(
+                  swerve,
+                  Arrays.stream(CamerasList.CAMERAS)
+                      .map(
+                          (camera) ->
+                              new VisionIOPhotonVision(camera.name(), camera.robotToCamera()))
+                      .toArray(VisionIO[]::new));
         }
       }
 
@@ -92,15 +92,14 @@ public class RobotContainer {
                 new ModuleIOSim(TunerConstants.BackLeft),
                 new ModuleIOSim(TunerConstants.BackRight));
 
-        vision = 
+        vision =
             new Vision(
                 swerve,
                 Arrays.stream(CamerasList.CAMERAS)
-                    .map((camera) ->
-                        new VisionIOPhotonVisionSim(
-                            camera.name(),
-                            camera.robotToCamera(),
-                            swerve::getPose))
+                    .map(
+                        (camera) ->
+                            new VisionIOPhotonVisionSim(
+                                camera.name(), camera.robotToCamera(), swerve::getPose))
                     .toArray(VisionIO[]::new));
       }
 
@@ -114,12 +113,12 @@ public class RobotContainer {
                 new ModuleIO() {},
                 new ModuleIO() {});
 
-        vision = new Vision(
-            swerve,
+        vision =
+            new Vision(
+                swerve,
                 Arrays.stream(CamerasList.CAMERAS)
                     .map((camera) -> new VisionIO() {})
-                    .toArray(VisionIO[]::new)
-        );
+                    .toArray(VisionIO[]::new));
       }
     }
 
--- src/main/java/com/stuypulse/robot/subsystems/vision/VisionIOLimelight.java
+++ src/main/java/com/stuypulse/robot/subsystems/vision/VisionIOLimelight.java
@@ -12,9 +12,12 @@
 // GNU General Public License for more details.
 package com.stuypulse.robot.subsystems.vision;
 
+import static edu.wpi.first.units.Units.Radians;
+
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.DoubleArrayPublisher;
 import edu.wpi.first.networktables.DoubleArraySubscriber;
@@ -22,7 +25,6 @@ import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.IntegerPublisher;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.RobotController;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -34,6 +36,7 @@ import java.util.function.Supplier;
 public class VisionIOLimelight implements VisionIO {
   private final Supplier<Rotation2d> rotationSupplier;
   private final DoubleArrayPublisher orientationPublisher;
+  private final DoubleArrayPublisher cameraPosePublisher;
   private final IntegerPublisher pipelinePublisher;
   private final IntegerPublisher imuModePublisher;
   private final DoubleArrayPublisher aprilTagWhitelistPublisher;
@@ -52,12 +55,14 @@ public class VisionIOLimelight implements VisionIO {
    * @param name The configured name of the Limelight.
    * @param rotationSupplier Supplier for the current estimated rotation, used for MegaTag 2.
    */
-  public VisionIOLimelight(String name, Supplier<Rotation2d> rotationSupplier) {
+  public VisionIOLimelight(
+      String name, Transform3d robotToCamera, Supplier<Rotation2d> rotationSupplier) {
     megaTagMode = MegaTagMode.MEGATAG_1;
 
     var table = NetworkTableInstance.getDefault().getTable(name);
     this.rotationSupplier = rotationSupplier;
     pipelinePublisher = table.getIntegerTopic("pipeline").publish();
+    cameraPosePublisher = table.getDoubleArrayTopic("camerapose_robotspace_set").publish();
     imuModePublisher = table.getIntegerTopic("imumode_set").publish();
     orientationPublisher = table.getDoubleArrayTopic("robot_orientation_set").publish();
     aprilTagWhitelistPublisher = table.getDoubleArrayTopic("fiducial_id_filters_set").publish();
@@ -67,6 +72,16 @@ public class VisionIOLimelight implements VisionIO {
     megatag1Subscriber = table.getDoubleArrayTopic("botpose_wpiblue").subscribe(new double[] {});
     megatag2Subscriber =
         table.getDoubleArrayTopic("botpose_orb_wpiblue").subscribe(new double[] {});
+
+    cameraPosePublisher.accept(
+        new double[] {
+          robotToCamera.getX(),
+          robotToCamera.getY(),
+          robotToCamera.getZ(),
+          robotToCamera.getRotation().getMeasureX().in(Radians),
+          robotToCamera.getRotation().getMeasureY().in(Radians),
+          robotToCamera.getRotation().getMeasureZ().in(Radians)
+        });
   }
 
   @Override
@@ -166,7 +181,8 @@ public class VisionIOLimelight implements VisionIO {
 
     pipelinePublisher.accept(outputs.pipeline);
 
-    aprilTagWhitelistPublisher.accept(Arrays.stream(outputs.aprilTagIDWhitelist).asDoubleStream().toArray());
+    aprilTagWhitelistPublisher.accept(
+        Arrays.stream(outputs.aprilTagIDWhitelist).asDoubleStream().toArray());
 
     imuModePublisher.accept(outputs.imuMode);
   }
